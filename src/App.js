bd57c5cd0348f8a83f92224965e2221406805cec
VinhTDC <22211TT4921@mail.tdc.edu.vn>
2024-04-04 06:37:56 +0700

    add-about-customer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import MapEmbed from './components/MapEmbed'; 
+import Customer from './components/Customer';
+import About from './components/About';
 function App() {
   return (
     <div>
+      <Customer />
+      <About />
       <MapEmbed />
     </div>
   );
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,5 +24,4 @@ const About = () => {
         </div>
     );
 };
-
 export default About;
