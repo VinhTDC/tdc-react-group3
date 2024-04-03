a779eb47f7c0be8d65176186c4f1ceca71c97053
VinhTDC <22211TT4921@mail.tdc.edu.vn>
2024-04-03 22:24:07 +0700

    Fix conflict

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import MapEmbed from "./components/MapEmbed";
-import './App.css';
-import Banner from './components/banner';
-
+import Banner  from "./components/banner";
 function App() {
   return (
     <div>
-      {/* Banner */}
       <Banner />
       <Header />
       <MapEmbed />
     </div>
   );
 }
-
-
-
+export default App;
