d025d7ce0d27103960f46dcea9c4a2dbbdb14469
An <dinhanngo058@gmail.com>
2024-04-04 07:07:48 +0700

    Footer

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 import Header from "./components/Header";
 import MapEmbed from "./components/MapEmbed";
 import Banner from "./components/banner";
-import About from "./components/About"; 
-import Customer from "./components/Customer" ;
+import About from "./components/About";
+import Customer from "./components/Customer";
+import Footer from "./components/footer";
 function App() {
   return (
     <div>
-      <Banner />
       <Header />
+      <Banner />
       <About />
-      <Customer/>
+      <Customer />
       <MapEmbed />
-      
+      <Footer />
+
     </div>
   );
 }
